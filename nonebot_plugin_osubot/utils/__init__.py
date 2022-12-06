e81134775f136d9a85c58941fffe51c80c8b1ff8
yaowan233 <572473053@qq.com>
2022-12-06 20:15:31 +0800

    :bug: 修复一个存在好久的bug，修复了数据库的更新操作（我就是笨蛋）

--- nonebot_plugin_osubot/utils/__init__.py
+++ nonebot_plugin_osubot/utils/__init__.py
@@ -13,21 +13,39 @@ async def update_user_info(uid: int):
         userinfo_dic = await osu_api('update', uid, GM[mode])
         userinfo = User(**userinfo_dic)
         if userinfo.statistics.play_count != 0:
-            await InfoData.update_or_create(osu_id=uid,
-                                            c_rank=userinfo.statistics.country_rank,
-                                            g_rank=userinfo.statistics.global_rank,
-                                            pp=userinfo.statistics.pp,
-                                            acc=round(userinfo.statistics.hit_accuracy, 2),
-                                            pc=userinfo.statistics.play_count,
-                                            count=userinfo.statistics.total_hits,
-                                            osu_mode=mode)
+            if info := await InfoData.filter(osu_id=uid, osu_mode=mode).first():
+                info.c_rank = userinfo.statistics.country_rank
+                info.g_rank = userinfo.statistics.global_rank
+                info.pp = userinfo.statistics.pp
+                info.acc = round(userinfo.statistics.hit_accuracy, 2)
+                info.pc = userinfo.statistics.play_count
+                info.count = userinfo.statistics.total_hits
+            else:
+                info = InfoData(osu_id=uid,
+                                c_rank=userinfo.statistics.country_rank,
+                                g_rank=userinfo.statistics.global_rank,
+                                pp=userinfo.statistics.pp,
+                                acc=round(userinfo.statistics.hit_accuracy, 2),
+                                pc=userinfo.statistics.play_count,
+                                count=userinfo.statistics.total_hits,
+                                osu_mode=mode)
         else:
-            await InfoData.update_or_create(osu_id=uid,
-                                            c_rank=0,
-                                            g_rank=0,
-                                            pp=0,
-                                            acc=0,
-                                            pc=0,
-                                            count=0,
-                                            osu_mode=mode)
+            if info := await InfoData.filter(osu_id=uid, osu_mode=mode).first():
+                info.c_rank = userinfo.statistics.country_rank
+                info.g_rank = userinfo.statistics.global_rank
+                info.pp = userinfo.statistics.pp
+                info.acc = round(userinfo.statistics.hit_accuracy, 2)
+                info.pc = userinfo.statistics.play_count
+                info.count = userinfo.statistics.total_hits
+            else:
+                info = InfoData(osu_id=uid,
+                                c_rank=0,
+                                g_rank=0,
+                                pp=0,
+                                acc=0,
+                                pc=0,
+                                count=0,
+                                osu_mode=mode)
+        await info.save()
         logger.info(f'玩家:[{userinfo.username}] {GM[mode]}模式 个人信息更新完毕')
+
