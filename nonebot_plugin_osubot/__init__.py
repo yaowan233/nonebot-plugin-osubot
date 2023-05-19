eeb9028afcbd9ef87ceab1af84a2fd5372c3c52c
yaowan233 <572473053@qq.com>
2023-05-19 12:55:10 +0800

    :rocket: v1.7.5

--- nonebot_plugin_osubot/__init__.py
+++ nonebot_plugin_osubot/__init__.py
@@ -68,7 +68,7 @@ __plugin_meta__ = PluginMetadata(
     extra={
         "unique_name": "osubot",
         "author": "yaowan233 <572473053@qq.com>",
-        "version": "1.7.4",
+        "version": "1.7.5",
     },
 )
 
