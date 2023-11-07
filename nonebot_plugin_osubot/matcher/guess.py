f21a7830bd12d6a2149922310a5f600ec6a8ab3c
yaowan233 <572473053@qq.com>
2023-11-07 14:52:33 +0800

    :sparkles: red适配器增加音频猜歌功能

--- nonebot_plugin_osubot/matcher/guess.py
+++ nonebot_plugin_osubot/matcher/guess.py
@@ -3,6 +3,7 @@ import random
 from asyncio import TimerHandle
 from difflib import SequenceMatcher
 from io import BytesIO
+from pathlib import Path
 from typing import Dict, Union
 from PIL import Image
 
@@ -27,6 +28,7 @@ from ..utils import NGM
 from ..api import osu_api
 from ..schema import Score
 from ..database.models import UserData
+from ..api import safe_async_get
 
 games: Dict[int, Score] = {}
 pic_games: Dict[int, Score] = {}
@@ -38,6 +40,8 @@ group_hint = {}
 pic_group_hint = {}
 guess_audio = on_command("音频猜歌", priority=11, block=True)
 guess_song_cache = ExpiringDict(1000, 60 * 60 * 24)
+data_path = Path() / "data" / "osu"
+pcm_path = data_path / "out.pcm"
 
 
 async def get_random_beatmap_set(binded_id, group_id, ttl=10):
@@ -97,6 +101,39 @@ async def _(state: T_State, event: v11GroupMessageEvent, bot: v11Bot, matcher: M
     )
 
 
+@guess_audio.handle(parameterless=[split_msg()])
+async def _(state: T_State, event: RedGroupMessageEvent, bot: RedBot, matcher: Matcher):
+    if "error" in state:
+        await guess_audio.finish(
+            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
+            + state["error"]
+        )
+    mode = state["mode"]
+    group_id = int(event.group_id)
+    # group_member = await bot.get_group_member_list(group_id=group_id)
+    # user_id_ls = [i["user_id"] for i in group_member]
+    binded_id = await UserData.filter(osu_mode=mode).values_list("user_id", flat=True)
+    if not binded_id:
+        await guess_audio.finish("群里还没有人绑定该模式的osu账号呢，绑定了再来试试吧")
+    if not guess_song_cache.get(group_id):
+        guess_song_cache[group_id] = set()
+    selected_score, selected_user = await get_random_beatmap_set(binded_id, group_id)
+    if not selected_score:
+        await guess_audio.finish("好像没有可以猜的歌了，今天的猜歌就到此结束吧！")
+    if games.get(group_id, None):
+        await guess_audio.finish("现在还有进行中的猜歌呢，请等待当前猜歌结束")
+    games[group_id] = selected_score
+    set_timeout(matcher, group_id)
+    # group_member = await bot.get_group_member_info(
+    #     group_id=event.group_id, user_id=selected_user.user_id
+    # )
+    # name = group_member["card"] or group_member.get("nickname", "")
+    await guess_audio.send(f"开始音频猜歌游戏，猜猜下面音频的曲名吧，该曲抽选自{selected_user.user_id}的bp")
+    print(selected_score.beatmapset.title)
+    await download_audio(selected_score.beatmapset.id)
+    await pic_hint.finish(RedMessageSegment.voice(Path("out.silk")))
+
+
 async def stop_game(matcher: Matcher, cid: int):
     timers.pop(cid, None)
     if games.get(cid, None):
@@ -143,8 +180,8 @@ def pic_set_timeout(matcher: Matcher, cid: int, timeout: float = 300):
     pic_timers[cid] = timer
 
 
-def game_running(event: v11GroupMessageEvent) -> bool:
-    return bool(games.get(event.group_id, None))
+def game_running(event: Union[v11GroupMessageEvent, RedGroupMessageEvent]) -> bool:
+    return bool(games.get(int(event.group_id), None))
 
 
 def pic_game_running(event: Union[v11GroupMessageEvent, RedGroupMessageEvent]) -> bool:
@@ -171,6 +208,24 @@ async def _(event: v11GroupMessageEvent):
             group_hint[event.group_id] = None
         msg = f"恭喜{event.sender.nickname}猜出正确答案为{song_name_unicode}"
         await word_matcher.finish(v11MessageSegment.reply(event.message_id) + msg)
+
+
+@word_matcher.handle()
+async def _(event: RedGroupMessageEvent):
+    song_name = games[int(event.group_id)].beatmapset.title
+    song_name_unicode = games[int(event.group_id)].beatmapset.title_unicode
+    r1 = SequenceMatcher(None, song_name.lower(), event.get_plaintext().lower()).ratio()
+    r2 = SequenceMatcher(
+        None, song_name_unicode.lower(), event.get_plaintext().lower()
+    ).ratio()
+    if r1 >= 0.5 or r2 >= 0.5:
+        games.pop(int(event.group_id))
+        if group_hint.get(event.group_id, None):
+            group_hint[event.group_id] = None
+        msg = f"恭喜{event.senderUin}猜出正确答案为{song_name_unicode}"
+        await word_matcher.finish(
+            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid) + msg
+        )
 
 
 @pic_word_matcher.handle()
@@ -236,6 +291,33 @@ async def _(event: v11GroupMessageEvent):
         await hint.finish(f"谱师为：{score.beatmapset.creator}")
 
 
+@hint.handle()
+async def _(event: RedGroupMessageEvent):
+    score = games[int(event.group_id)]
+    if not group_hint.get(event.group_id, None):
+        group_hint[event.group_id] = hint_dic.copy()
+    if all(group_hint[event.group_id].values()):
+        await hint.finish("已无更多提示，加油哦")
+    true_keys = []
+    for key, value in group_hint[event.group_id].items():
+        if not value:
+            true_keys.append(key)
+    action = random.choice(true_keys)
+    if action == "pic":
+        group_hint[event.group_id]["pic"] = True
+        data = await safe_async_get(score.beatmapset.covers.cover)
+        await hint.finish(RedMessageSegment.image(data.content))
+    if action == "artist":
+        group_hint[event.group_id]["artist"] = True
+        msg = f"曲师为：{score.beatmapset.artist_unicode}"
+        if score.beatmapset.artist_unicode != score.beatmapset.artist:
+            msg += f" [{score.beatmapset.artist}]"
+        await hint.finish(msg)
+    if action == "creator":
+        group_hint[event.group_id]["creator"] = True
+        await hint.finish(f"谱师为：{score.beatmapset.creator}")
+
+
 pic_hint = on_command("图片提示", priority=11, block=True, rule=Rule(pic_game_running))
 
 
@@ -283,7 +365,8 @@ async def _(event: RedGroupMessageEvent):
     action = random.choice(true_keys)
     if action == "audio":
         pic_group_hint[event.group_id]["audio"] = True
-        await pic_hint.finish("暂时发不了音频了 （＞人＜；）")
+        await download_audio(score.beatmapset.id)
+        await pic_hint.finish(RedMessageSegment.voice(Path("out.silk")))
     if action == "artist":
         pic_group_hint[event.group_id]["artist"] = True
         msg = f"曲师为：{score.beatmapset.artist_unicode}"
@@ -383,3 +466,19 @@ async def _(state: T_State, event: RedGroupMessageEvent, bot: RedBot, matcher: M
     cropped_image.save(byt, "png")
     print(selected_score.beatmapset.title_unicode)
     await guess_pic.finish(RedMessageSegment.image(byt))
+
+
+async def download_audio(set_id):
+    audio_path = data_path / f"{set_id}.mp3"
+    url = f"https://cdn.sayobot.cn:25225/preview/{set_id}.mp3"
+    data = await safe_async_get(url)
+    with open(audio_path, "wb") as file:
+        file.write(data.content)
+    process = await asyncio.create_subprocess_shell(
+        f"ffmpeg -y -i {audio_path} -f s16le -acodec pcm_s16le -ar 12500 -ac 2 -loglevel quiet {pcm_path}"
+    )
+    await process.wait()
+    task = await asyncio.create_subprocess_shell(
+        f"silk_v3_encoder.exe {pcm_path} out.silk -tencent -quiet"
+    )
+    await task.wait()
