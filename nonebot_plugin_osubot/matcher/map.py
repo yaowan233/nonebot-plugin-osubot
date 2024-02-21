f4d8ae1383089b464d11eacb2a5afd1d4870196a
A M D <52590027+mas-alone@users.noreply.github.com>
2024-02-22 01:26:17 +0800

    `url_match` 调用 `map`绘制

--- nonebot_plugin_osubot/matcher/map.py
+++ nonebot_plugin_osubot/matcher/map.py
@@ -1,101 +1,46 @@
 from typing import Union
 
-from nonebot import on_command
+from nonebot import on_regex
+from .map import osu_map
+from ..draw import draw_map_info
 from nonebot.adapters.red import (
-    MessageSegment as RedMessageSegment,
     MessageEvent as RedMessageEvent,
+    MessageSegment as RedMessageSegment,
 )
 from nonebot.adapters.onebot.v11 import (
     MessageEvent as v11MessageEvent,
     MessageSegment as v11MessageSegment,
 )
 from nonebot.typing import T_State
+from nonebot.params import RegexGroup
 from nonebot_plugin_guild_patch import GuildMessageEvent
 
-from .utils import split_msg
-from ..draw import draw_map_info, draw_bmap_info
-
-osu_map = on_command("map", priority=11, block=True)
-bmap = on_command("bmap", priority=11, block=True)
+url_match = on_regex("https://osu.ppy.sh/beatmapsets/([0-9]+)#([^/]+/[0-9]+)")
+url_1 = "https://osu.direct/api/d/"
+url_2 = "https://txy1.sayobot.cn/beatmaps/download/novideo/"
 
 
-@osu_map.handle(parameterless=[split_msg()])
-async def _map(state: T_State, event: Union[v11MessageEvent, GuildMessageEvent]):
-    map_id = state["para"]
-    mods = state["mods"]
-    if not map_id:
-        await osu_map.finish(v11MessageSegment.reply(event.message_id) + "请输入地图ID")
-    elif not map_id.isdigit():
-        await osu_map.finish(v11MessageSegment.reply(event.message_id) + "请输入正确的地图ID")
-    m = await draw_map_info(map_id, mods)
-    if isinstance(m, str):
-        await osu_map.finish(v11MessageSegment.reply(event.message_id) + m)
-    await osu_map.finish(
-        v11MessageSegment.reply(event.message_id) + v11MessageSegment.image(m)
-    )
+@url_match.handle()
+@osu_map.handle()
+async def _url(
+    state: T_State, event: Union[v11MessageEvent, GuildMessageEvent], bid: tuple = RegexGroup()
+):
+    beatmap_id = bid[1].split('/')[1]
+    url_total = f"kitsu镜像站：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
+    state['para'] = beatmap_id
+    state['mods'] = ''
+    m = await draw_map_info(beatmap_id, state['mods'])
+    await url_match.finish(v11MessageSegment.reply(event.message_id) + v11MessageSegment.image(m) + '\n' + url_total)
 
 
-@osu_map.handle(parameterless=[split_msg()])
-async def _map(state: T_State, event: RedMessageEvent):
-    map_id = state["para"]
-    mods = state["mods"]
-    if not map_id:
-        await osu_map.finish(
-            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
-            + "请输入地图ID"
-        )
-    elif not map_id.isdigit():
-        await osu_map.finish(
-            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
-            + "请输入正确的地图ID"
-        )
-    m = await draw_map_info(map_id, mods)
-    if isinstance(m, str):
-        await osu_map.finish(
-            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid) + m
-        )
-    await osu_map.finish(
-        RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
-        + RedMessageSegment.image(m)
-    )
-
-
-@bmap.handle(parameterless=[split_msg()])
-async def _bmap(state: T_State, event: Union[v11MessageEvent, GuildMessageEvent]):
-    set_id = state["para"]
-    if not set_id:
-        await bmap.finish(v11MessageSegment.reply(event.message_id) + "请输入setID")
-    if not set_id.isdigit():
-        await bmap.finish(v11MessageSegment.reply(event.message_id) + "请输入正确的setID")
-        return
-    m = await draw_bmap_info(set_id)
-    if isinstance(m, str):
-        await bmap.finish(v11MessageSegment.reply(event.message_id) + m)
-    await bmap.finish(
-        v11MessageSegment.reply(event.message_id) + v11MessageSegment.image(m)
-    )
-
-
-@bmap.handle(parameterless=[split_msg()])
-async def _bmap(state: T_State, event: RedMessageEvent):
-    set_id = state["para"]
-    if not set_id:
-        await bmap.finish(
-            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
-            + "请输入setID"
-        )
-    if not set_id.isdigit():
-        await bmap.finish(
-            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
-            + "请输入正确的setID"
-        )
-        return
-    m = await draw_bmap_info(set_id)
-    if isinstance(m, str):
-        await bmap.finish(
-            RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid) + m
-        )
-    await bmap.finish(
-        RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid)
-        + RedMessageSegment.image(m)
+@url_match.handle()
+@osu_map.handle()
+async def _url(state: T_State, event: RedMessageEvent, bid: tuple = RegexGroup()):
+    beatmap_id = bid[1].split('/')[1]
+    url_total = f"kitsu镜像站：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
+    state['para'] = beatmap_id
+    state['mods'] = ''
+    m = await draw_map_info(beatmap_id, state['mods'])
+    await url_match.finish(
+        RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid) + RedMessageSegment.image(m) + '\n' + url_total
     )
