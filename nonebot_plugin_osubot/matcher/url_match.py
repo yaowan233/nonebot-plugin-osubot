6d79c417ae7956ea2079c8fbaff36bbaa0544cf6
A M D <52590027+mas-alone@users.noreply.github.com>
2024-02-22 10:12:24 +0800

    :sparkles: `url_match` 调用 `map`绘制 (#43)spar

--- nonebot_plugin_osubot/matcher/url_match.py
+++ nonebot_plugin_osubot/matcher/url_match.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from nonebot import on_regex
+from .map import osu_map
+from ..draw import draw_map_info
 from nonebot.adapters.red import (
     MessageEvent as RedMessageEvent,
     MessageSegment as RedMessageSegment,
@@ -9,25 +11,36 @@ from nonebot.adapters.onebot.v11 import (
     MessageEvent as v11MessageEvent,
     MessageSegment as v11MessageSegment,
 )
+from nonebot.typing import T_State
 from nonebot.params import RegexGroup
 from nonebot_plugin_guild_patch import GuildMessageEvent
 
-url_match = on_regex("https://osu.ppy.sh/beatmapsets/(.*)#")
+url_match = on_regex("https://osu.ppy.sh/beatmapsets/([0-9]+)#([^/]+/[0-9]+)")
 url_1 = "https://osu.direct/api/d/"
 url_2 = "https://txy1.sayobot.cn/beatmaps/download/novideo/"
 
 
 @url_match.handle()
+@osu_map.handle()
 async def _url(
-    event: Union[v11MessageEvent, GuildMessageEvent], bid: tuple = RegexGroup()
+    state: T_State, event: Union[v11MessageEvent, GuildMessageEvent], bid: tuple = RegexGroup()
 ):
+    beatmap_id = bid[1].split('/')[1]
     url_total = f"kitsu镜像站：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
-    await url_match.finish(v11MessageSegment.reply(event.message_id) + url_total)
+    state['para'] = beatmap_id
+    state['mods'] = ''
+    m = await draw_map_info(beatmap_id, state['mods'])
+    await url_match.finish(v11MessageSegment.reply(event.message_id) + v11MessageSegment.image(m) + '\n' + url_total)
 
 
 @url_match.handle()
-async def _url(event: RedMessageEvent, bid: tuple = RegexGroup()):
+@osu_map.handle()
+async def _url(state: T_State, event: RedMessageEvent, bid: tuple = RegexGroup()):
+    beatmap_id = bid[1].split('/')[1]
     url_total = f"kitsu镜像站：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
+    state['para'] = beatmap_id
+    state['mods'] = ''
+    m = await draw_map_info(beatmap_id, state['mods'])
     await url_match.finish(
-        RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid) + url_total
+        RedMessageSegment.reply(event.msgSeq, event.msgId, event.senderUid) + RedMessageSegment.image(m) + '\n' + url_total
     )
