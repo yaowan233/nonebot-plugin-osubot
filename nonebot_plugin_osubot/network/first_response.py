dbdf458642cbf598ad80bff1174d2a24eb433494
yaowan233 <572473053@qq.com>
2024-04-07 15:06:33 +0800

    :bug: 修正网络异步获取的逻辑

--- nonebot_plugin_osubot/network/first_response.py
+++ nonebot_plugin_osubot/network/first_response.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import List
 
-from httpx import NetworkError, AsyncClient
+from httpx import AsyncClient
 from nonebot import get_plugin_config
 from ..config import Config
 
@@ -12,20 +12,19 @@ proxy = plugin_config.osu_proxy
 async def fetch_url(client: AsyncClient, url):
     try:
         response = await client.get(url)
-        if response.status_code == 200:
-            return response.content
-        else:
-            return None
+        return response
     except Exception:
         return None
 
 
 async def get_first_response(urls: List[str]):
     async with AsyncClient(proxies=proxy, follow_redirects=True) as client:
-        tasks = [fetch_url(client, url) for url in urls]
-        done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-        for task in done:
-            response = task.result()
-            if response is not None:
-                return response
+        tasks = [asyncio.create_task(fetch_url(client, url)) for url in urls]
+        while tasks:
+            done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+            for task in done:
+                response = task.result()
+                if response is not None and response.status_code == 200:
+                    return response.content
+            tasks = [task for task in tasks if not task.done()]
         return None
