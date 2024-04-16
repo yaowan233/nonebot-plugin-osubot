0a62761a19845e56738f0c6c6dd3bd1f462e8f86
yaowan233 <572473053@qq.com>
2024-04-16 13:34:03 +0800

    :sparkles: medal命令使用UniMessage发送

--- nonebot_plugin_osubot/matcher/medal.py
+++ nonebot_plugin_osubot/matcher/medal.py
@@ -1,16 +1,10 @@
 import re
 import json
-from typing import List, Tuple
 from pathlib import Path
 
+from nonebot_plugin_alconna import UniMessage
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import (
-    Message,
-    MessageSegment,
-    Bot,
-    MessageEvent,
-    GroupMessageEvent,
-)
+from nonebot.internal.adapter import Message
 from nonebot.params import CommandArg
 from ..api import safe_async_get
 
@@ -20,49 +14,11 @@ with open(medal_data_path, "r") as file:
     medal_json = json.load(file)
 
 
-async def send_forward_msg(
-    bot: Bot,
-    event: MessageEvent,
-    user_message: List[Tuple[str, str, Message]],
-):
-    """
-    发送 forward 消息
-
-    > 参数：
-        - bot: Bot 对象
-        - event: MessageEvent 对象
-        - user_message: 合并消息的用户信息列表
-
-    > 返回值：
-        - 成功：返回消息发送结果
-        - 失败：抛出异常
-    """
-
-    def to_json(info: Tuple[str, str, Message]):
-        """
-        将消息转换为 forward 消息的 json 格式
-        """
-        return {
-            "type": "node",
-            "data": {"name": info[0], "uin": info[1], "content": info[2]},
-        }
-
-    messages = [to_json(info) for info in user_message]
-    if isinstance(event, GroupMessageEvent):
-        return await bot.call_api(
-            "send_group_forward_msg", group_id=event.group_id, messages=messages
-        )
-    else:
-        return await bot.call_api(
-            "send_private_forward_msg", user_id=event.user_id, messages=messages
-        )
-
-
 medal = on_command("medal", aliases={"成就"}, priority=11, block=True)
 
 
 @medal.handle()
-async def _(bot: Bot, event: MessageEvent, msg: Message = CommandArg()):
+async def _(msg: Message = CommandArg()):
     name = msg.extract_plain_text()
     data = await safe_async_get(
         f"https://osekai.net/medals/api/public/get_medal.php?medal={name}"
@@ -113,20 +69,14 @@ async def _(bot: Bot, event: MessageEvent, msg: Message = CommandArg()):
         words += (
             f'\nhttps://osu.ppy.sh/beatmaps/packs/{medal_data["PackID"].rstrip(",,,")}'
         )
-    await medal.send(
-        MessageSegment.reply(event.message_id)
-        + MessageSegment.image(medal_data["Link"])
-        + words
-    )
+    await (UniMessage.image(url=medal_data["Link"]) + words).send(reply_to=True)
     if medal_data["beatmaps"]:
-        msg_ls = []
+        msg = UniMessage()
+        if len(medal_data["beatmaps"]) > 5:
+            medal_data["beatmaps"] = medal_data["beatmaps"][:5]
         for beatmap in medal_data["beatmaps"]:
-            msg = (
-                MessageSegment.image(
-                    f'https://assets.ppy.sh/beatmaps/{beatmap["MapsetID"]}/covers/cover.jpg'
-                )
-                + f'{beatmap["SongTitle"]} [{beatmap["DifficultyName"]}]\n{beatmap["Difficulty"]}⭐\n'
+            msg.append(
+                f'{beatmap["SongTitle"]} [{beatmap["DifficultyName"]}]\n{beatmap["Difficulty"]}⭐\n'
                 + f'https://osu.ppy.sh/b/{beatmap["BeatmapID"]}'
             )
-            msg_ls.append(("推荐谱面", bot.self_id, msg))
-        await send_forward_msg(bot, event, msg_ls)
+        await msg.send()
