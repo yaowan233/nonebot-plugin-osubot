98f4d71f4a4c20c0853e463bde3f9f32b6a4e570
yaowan233 <572473053@qq.com>
2024-05-14 14:21:29 +0800

    :bug: 修复lazer显示问题

--- nonebot_plugin_osubot/matcher/pr.py
+++ nonebot_plugin_osubot/matcher/pr.py
@@ -3,9 +3,11 @@ from nonebot.internal.adapter import Event
 from nonebot_plugin_alconna import UniMessage
 from nonebot.params import T_State
 
+from ..database import UserData
 from ..api import get_user_info
 from .utils import split_msg
 from ..draw import draw_score
+from ..draw.score import cal_legacy_acc, cal_legacy_rank
 from ..schema import NewScore
 from ..api import osu_api
 from ..draw.bp import draw_pfm
@@ -18,8 +20,7 @@ pr = on_command("pr", priority=11, block=True, aliases={"PR", "Pr", "pR"})
 @recent.handle(parameterless=[split_msg()])
 async def _recent(event: Event, state: T_State):
     if "error" in state:
-        await UniMessage.text(state["error"]).send(reply_to=True)
-        return
+        await UniMessage.text(state["error"]).finish(reply_to=True)
     mode = NGM[state["mode"]]
     if "-" in state["para"]:
         ls = state["para"].split("-")
@@ -33,38 +34,52 @@ async def _recent(event: Event, state: T_State):
             limit=high,
         )
         if not data:
-            await UniMessage.text(f"未查询到在 {mode} 的游玩记录").send(reply_to=True)
-            return
+            await UniMessage.text(f"未查询到在 {mode} 的游玩记录").finish(reply_to=True)
         if isinstance(data, str):
-            await UniMessage.text(data).send(reply_to=True)
+            await UniMessage.text(data).finish(reply_to=True)
         if not state["is_name"]:
             info = await get_user_info(
                 f"https://osu.ppy.sh/api/v2/users/{state['user']}?key=id"
             )
             if isinstance(info, str):
-                return info
+                await UniMessage.text(info).finish(reply_to=True)
             else:
                 state["user"] = info["username"]
         score_ls = [NewScore(**score_json) for score_json in data]
+        player = await UserData.get_or_none(user_id=int(event.get_user_id()))
+        if not player.lazer_mode:
+            score_ls = [i for i in score_ls if {"acronym": "CL"} in i.mods]
+        for score_info in score_ls:
+            if not player.lazer_mode:
+                score_info.mods.remove({"acronym": "CL"}) if {
+                    "acronym": "CL"
+                } in score_info.mods else None
+            if score_info.ruleset_id == 3 and not player.lazer_mode:
+                score_info.accuracy = cal_legacy_acc(score_info.statistics)
+                is_hidden = {"acronym": "HD"} in score_info.mods
+                score_info.rank = cal_legacy_rank(score_info.accuracy, is_hidden)
         pic = await draw_pfm("relist", state["user"], score_ls, score_ls, mode)
-        await UniMessage.image(raw=pic).send(reply_to=True)
-        return
+        await UniMessage.image(raw=pic).finish(reply_to=True)
     if state["day"] == 0:
         state["day"] = 1
     data = await draw_score(
-        "recent", state["user"], int(event.get_user_id()), mode, [], state["day"] - 1, is_name=state["is_name"]
+        "recent",
+        state["user"],
+        int(event.get_user_id()),
+        mode,
+        [],
+        state["day"] - 1,
+        is_name=state["is_name"],
     )
     if isinstance(data, str):
-        await UniMessage.text(data).send(reply_to=True)
-        return
-    await UniMessage.image(raw=data).send(reply_to=True)
+        await UniMessage.text(data).finish(reply_to=True)
+    await UniMessage.image(raw=data).finish(reply_to=True)
 
 
 @pr.handle(parameterless=[split_msg()])
 async def _pr(event: Event, state: T_State):
     if "error" in state:
-        await UniMessage.text(state["error"]).send(reply_to=True)
-        return
+        await UniMessage.text(state["error"]).finish(reply_to=True)
     mode = state["mode"]
     if "-" in state["para"]:
         ls = state["para"].split("-")
@@ -90,12 +105,30 @@ async def _pr(event: Event, state: T_State):
             else:
                 state["user"] = info["username"]
         score_ls = [NewScore(**score_json) for score_json in data]
+        player = await UserData.get_or_none(user_id=int(event.get_user_id()))
+        if not player.lazer_mode:
+            score_ls = [i for i in score_ls if {"acronym": "CL"} in i.mods]
+        for score_info in score_ls:
+            if not player.lazer_mode:
+                score_info.mods.remove({"acronym": "CL"}) if {
+                    "acronym": "CL"
+                } in score_info.mods else None
+            if score_info.ruleset_id == 3 and not player.lazer_mode:
+                score_info.accuracy = cal_legacy_acc(score_info.statistics)
+                is_hidden = {"acronym": "HD"} in score_info.mods
+                score_info.rank = cal_legacy_rank(score_info.accuracy, is_hidden)
         pic = await draw_pfm("prlist", state["user"], score_ls, score_ls, NGM[mode])
         await UniMessage.image(raw=pic).finish(reply_to=True)
     if state["day"] == 0:
         state["day"] = 1
     data = await draw_score(
-        "pr", state["user"], int(event.get_user_id()), NGM[mode], [], state["day"] - 1, is_name=state["is_name"]
+        "pr",
+        state["user"],
+        int(event.get_user_id()),
+        NGM[mode],
+        [],
+        state["day"] - 1,
+        is_name=state["is_name"],
     )
     if isinstance(data, str):
         await UniMessage.text(data).finish(reply_to=True)
