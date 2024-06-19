5d98ea11cd6507c59523f5ee346ac00d8bfeb775
yaowan233 <572473053@qq.com>
2024-06-19 18:59:42 +0800

    :bug: 创建文件夹

--- nonebot_plugin_osubot/draw/utils.py
+++ nonebot_plugin_osubot/draw/utils.py
@@ -239,6 +239,8 @@ def calc_songlen(length: int) -> str:
 
 async def open_user_icon(info: User) -> Image:
     path = user_cache_path / str(info.id)
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
     for file_path in path.glob("icon*.*"):
         # 检查文件是否为图片格式
         if file_path.suffix.lower() in [".jpg", ".png", ".jpeg", ".gif", ".bmp"]:
