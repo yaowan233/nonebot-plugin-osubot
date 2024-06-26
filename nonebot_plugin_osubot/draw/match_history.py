833335263dbb56cd933d7f0dfa246e77a5143408
A M D <52590027+mas-alone@users.noreply.github.com>
2024-06-26 19:59:58 +0800

    ruff

--- nonebot_plugin_osubot/draw/match_history.py
+++ nonebot_plugin_osubot/draw/match_history.py
@@ -3,12 +3,23 @@ from playwright.async_api import ViewportSize
 
 
 async def draw_match_history(match_id: str) -> bytes:
-    async with get_new_page(viewport=ViewportSize(width=900, height=1), extra_http_headers={"Accept-Language": "zh-CN"}) as page:
-        await page.goto(f"https://osu.ppy.sh/community/matches/{match_id}", wait_until="networkidle")
-        await page.add_style_tag(content='''
+    async with get_new_page(
+        viewport=ViewportSize(width=900, height=1),
+        extra_http_headers={"Accept-Language": "zh-CN"}
+    ) as page:
+        await page.goto(
+            f"https://osu.ppy.sh/community/matches/{match_id}",
+            wait_until="networkidle"
+        )
+        await page.add_style_tag(content="""
             .mp-history-event, .mp-history-content__item--event, .nav2, .audio-player {
                 display: none !important;
             }
-        ''')
-        pic = await page.screenshot(omit_background=True, full_page=True, quality=60, type="jpeg")
+        """)
+        pic = await page.screenshot(
+            omit_background=True,
+            full_page=True,
+            quality=60,
+            type="jpeg"
+        )
         return pic
