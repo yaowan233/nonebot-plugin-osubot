d15a4bc5f1eae7234d904025de4e1c7f59137111
A M D <52590027+mas-alone@users.noreply.github.com>
2024-06-27 08:49:47 +0800

    :sparkles: 修改match样式 (#62)
    * 修改match样式
    
    * ruff

--- nonebot_plugin_osubot/draw/match_history.py
+++ nonebot_plugin_osubot/draw/match_history.py
@@ -3,7 +3,23 @@ from playwright.async_api import ViewportSize
 
 
 async def draw_match_history(match_id: str) -> bytes:
-    async with get_new_page(viewport=ViewportSize(width=800, height=1)) as page:
-        await page.goto(f"https://osu.ppy.sh/community/matches/{match_id}", wait_until="networkidle")
-        pic = await page.screenshot(omit_background=True, full_page=True, quality=60, type="jpeg")
+    async with get_new_page(
+        viewport=ViewportSize(width=900, height=1),
+        extra_http_headers={"Accept-Language": "zh-CN"}
+    ) as page:
+        await page.goto(
+            f"https://osu.ppy.sh/community/matches/{match_id}",
+            wait_until="networkidle"
+        )
+        await page.add_style_tag(content="""
+            .mp-history-event, .mp-history-content__item--event, .nav2, .audio-player {
+                display: none !important;
+            }
+        """)
+        pic = await page.screenshot(
+            omit_background=True,
+            full_page=True,
+            quality=60,
+            type="jpeg"
+        )
         return pic
