5e7b80b26d13bf6426fca88bb412a31c77d68ac5
yaowan233 <572473053@qq.com>
2024-12-09 16:02:49 +0800

    :sparkles: 新增下载地址

--- nonebot_plugin_osubot/matcher/url_match.py
+++ nonebot_plugin_osubot/matcher/url_match.py
@@ -5,6 +5,7 @@ from nonebot_plugin_alconna import UniMessage
 from ..draw import draw_map_info
 
 url_match = on_regex("https://osu.ppy.sh/beatmapsets/([0-9]+)#([^/]+/[0-9]+)")
+url = "https://catboy.best/d/"
 url_1 = "https://osu.direct/api/d/"
 url_2 = "https://txy1.sayobot.cn/beatmaps/download/novideo/"
 
@@ -12,6 +13,6 @@ url_2 = "https://txy1.sayobot.cn/beatmaps/download/novideo/"
 @url_match.handle()
 async def _url(bid: tuple = RegexGroup()):
     beatmap_id = bid[1].split("/")[1]
-    url_total = f"kitsu镜像站：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
+    url_total = f"镜像站1：{url}{bid[0]}\n镜像站2：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
     m = await draw_map_info(beatmap_id, [], True)
     await (UniMessage.image(raw=m) + "\n" + url_total).finish(reply_to=True)
