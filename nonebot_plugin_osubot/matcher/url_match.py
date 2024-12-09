09d4bf184115487956cac34d33dc5c8a8c6d9964
yaowan233 <572473053@qq.com>
2024-12-09 16:11:48 +0800

    :sparkles: 新增下载地址

--- nonebot_plugin_osubot/matcher/url_match.py
+++ nonebot_plugin_osubot/matcher/url_match.py
@@ -14,5 +14,8 @@ url_2 = "https://txy1.sayobot.cn/beatmaps/download/novideo/"
 async def _url(bid: tuple = RegexGroup()):
     beatmap_id = bid[1].split("/")[1]
     url_total = f"镜像站1：{url}{bid[0]}\n镜像站2：{url_1}{bid[0]}\n小夜镜像站：{url_2}{bid[0]}"
-    m = await draw_map_info(beatmap_id, [], True)
+    try:
+        m = await draw_map_info(beatmap_id, [], True)
+    except Exception:
+        ...  # ignore
     await (UniMessage.image(raw=m) + "\n" + url_total).finish(reply_to=True)
