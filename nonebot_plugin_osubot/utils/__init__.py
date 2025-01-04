cb60edb1043c9dc25e5068787e31c87ff8e9cdc1
A M D <52590027+mas-alone@users.noreply.github.com>
2025-01-04 13:53:38 +0800

    Update __init__.py

--- nonebot_plugin_osubot/utils/__init__.py
+++ nonebot_plugin_osubot/utils/__init__.py
@@ -1,7 +1,7 @@
-GM = {0: "osu", 1: "taiko", 2: "fruits", 3: "mania"}
-NGM = {"0": "osu", "1": "taiko", "2": "fruits", "3": "mania"}
-GMN = {"osu": "Std", "taiko": "Taiko", "fruits": "Ctb", "mania": "Mania"}
-FGM = {"osu": 0, "taiko": 1, "fruits": 2, "mania": 3}
+GM = {0: "osu", 1: "taiko", 2: "fruits", 3: "mania", 4: "osu", 5: "taiko", 6: "fruits", 7: "mania"}
+NGM = {"0": "osu", "1": "taiko", "2": "fruits", "3": "mania", "4": "rxosu", "5": "rxtaiko", "6": "rxfruits", "7": "rxmania"}
+GMN = {"osu": "Std", "taiko": "Taiko", "fruits": "Ctb", "mania": "Mania", "rxosu": "RX Std", "rxtaiko": "RX Taiko", "rxfruits": "RX Ctb", "rxmania": "RX Mania"}
+FGM = {"osu": 0, "taiko": 1, "fruits": 2, "mania": 3, "rxosu": 4, "rxtaiko": 5, "rxfruits": 6, "rxmania": 7}
 
 
 def mods2list(args: str) -> list:
