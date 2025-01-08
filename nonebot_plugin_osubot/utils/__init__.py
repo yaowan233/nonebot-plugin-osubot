f853fed55b419dc6ef8844914047c6f32dfdf5d8
A M D <52590027+mas-alone@users.noreply.github.com>
2025-01-08 10:44:01 +0800

    添加 AP 支持

--- nonebot_plugin_osubot/utils/__init__.py
+++ nonebot_plugin_osubot/utils/__init__.py
@@ -1,4 +1,12 @@
-GM = {0: "osu", 1: "taiko", 2: "fruits", 3: "mania", 4: "osu", 5: "taiko", 6: "fruits"}
+GM = {0: "osu",
+      1: "taiko",
+      2: "fruits",
+      3: "mania",
+      4: "osu",
+      5: "taiko",
+      6: "fruits",
+      8: "osu",
+}
 NGM = {
     "0": "osu",
     "1": "taiko",
@@ -7,6 +15,7 @@ NGM = {
     "4": "rxosu",
     "5": "rxtaiko",
     "6": "rxfruits",
+    "8": "aposu",
 }
 GMN = {
     "osu": "Std",
@@ -16,8 +25,17 @@ GMN = {
     "rxosu": "RX Std",
     "rxtaiko": "RX Taiko",
     "rxfruits": "RX Ctb",
+    "aposu": "AP Std",
 }
-FGM = {"osu": 0, "taiko": 1, "fruits": 2, "mania": 3, "rxosu": 4, "rxtaiko": 5, "rxfruits": 6}
+FGM = {"osu": 0,
+       "taiko": 1,
+       "fruits": 2,
+       "mania": 3,
+       "rxosu": 4,
+       "rxtaiko": 5,
+       "rxfruits": 6,
+       "aposu": 8,
+}
 
 
 def mods2list(args: str) -> list:
