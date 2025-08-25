14108532dabf7f6ed52a6eb10a4c3b93f8d6a1bd
Jason House <52590027+mas-alone@users.noreply.github.com>
2025-08-26 00:09:56 +0800

    Update file.py

--- nonebot_plugin_osubot/file.py
+++ nonebot_plugin_osubot/file.py
@@ -47,18 +47,6 @@ async def download_map(setid: int) -> Optional[Path]:
     return filepath
 
 
-async def download_tmp_osu(map_id):
-    url = f"https://osu.ppy.sh/osu/{map_id}"
-    logger.info(f"开始下载谱面: <{map_id}>")
-    req = await safe_async_get(url)
-    filename = f"{map_id}.osu"
-    filepath = map_path / filename
-    chunk = req.read()
-    with open(filepath, "wb") as f:
-        f.write(chunk)
-    return filepath
-
-
 @auto_retry
 async def download_osu(set_id, map_id):
     url = [
